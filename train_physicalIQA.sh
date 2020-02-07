6ec8ed12b3c3ae7ed482660ce6b235d82c8f62df
Deniz Beser <denizbeser15@gmail.com>
2020-02-07 13:59:09 -0500

    slurm params

--- train_physicalIQA.sh
+++ train_physicalIQA.sh
@@ -1,19 +1,20 @@
+#!/bin/bash
+#SBATCH --account=mics
+#SBATCH --partition=mics
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=4g
+
 source ~/.bashrc
 conda activate mcs
 
-#!/bin/bash 
-#SBATCH --ntasks=1
-#SBATCH --time=10:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:p100:1
-#SBATCH --mem-per-cpu=4g
-#SBATCH --partition=isi
-
 MODEL_TYPE=roberta
-TASK_NAME=physicaliqa
+TASK_NAME=physicaliqa-carved
 #TASK_NAME2=cn_all_cs
 #TASK_NAME2=cn_all_cs_10k
-TASK_NAME2=cn_all_cs_20k
+#TASK_NAME2=cn_all_cs_20k
 #TASK_NAME2=cn_all_cs_50k
 #TASK_NAME2=cn_physical_cs_narrow
 #TASK_NAME2=cn_physical_cs_relaxed
@@ -29,9 +30,9 @@ python3 -W ignore train.py --model_type $MODEL_TYPE --model_weight $MODEL_WEIGHT
   --task_config_file config/tasks.yaml \
   --running_config_file config/physicalIQA.yaml \
   --task_name $TASK_NAME \
-  --task_name2 $TASK_NAME2 \
-  --task2_separate_fc true \
+#  --task_name2 $TASK_NAME2 \
+#  --task2_separate_fc true \
   --task_cache_dir ./cache \
   --output_dir output/$MODEL_TYPE-$MODEL_WEIGHT-$TASK_NAME-pred \
-  --output_dir2 output/$MODEL_TYPE-$MODEL_WEIGHT-$TASK_NAME2-pred \
+#  --output_dir2 output/$MODEL_TYPE-$MODEL_WEIGHT-$TASK_NAME2-pred \
   --log_save_interval 25 --row_log_interval 25
