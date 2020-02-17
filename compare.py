ce91721a409b47fb0b484e0adb39b9ec6acfb1d6
Nuan Wen <nuankw@yahoo.com>
2020-02-16 21:56:43 -0800

    better formatting

--- compare.py
+++ compare.py
@@ -5,13 +5,13 @@ and find where two predictions are inconsistent
 """
 
 import argparse
-
+import jsonlines
 def compare(label_path, jsonl_path,
             exp_name_1, pred_1_path, prob_1_path,
             exp_name_2, pred_2_path, prob_2_path):
     print(jsonl_path)
     labels = [int(i) for i in open(label_path).readlines()]
-    qa_reader = open(jsonl_path)
+    qa_reader = jsonlines.open(jsonl_path)
     pred_1 = [int(i) for i in open(pred_1_path).readlines()]
     prob_1 = [[float(j) for j in i.split()] for i in open(prob_1_path).readlines()]
     pred_2 = [int(i) for i in open(pred_2_path).readlines()]
@@ -22,23 +22,33 @@ def compare(label_path, jsonl_path,
     count_2_wrong = 0
     n_samples = len(labels)
     for i in range(len(labels)):
-        sample = qa_reader.readline()
+        sample = qa_reader.read()
         label = labels[i]
-        if pred_2[i] != label:
-            count_2_wrong += 1
         if pred_1[i] != label:
             count_1_wrong += 1
-	if pred_1[i] != pred_2[i]:
-            print("Disagreement on", str(i+1)+"-th sample:\n", sample.rstrip())
+        if pred_2[i] != label:
+            count_2_wrong += 1
+        if pred_1[i] != pred_2[i]:
+            print()
+            print("---------------------------")
+            print("Disagreement on", str(i+1)+"-th sample:")
+            print("\tContext:", sample["context"])
+            print("\tQuestion:", sample["question"])
+            print("\tAnswer choice 1:", sample["answerA"])
+            print("\tAnswer choice 2:", sample["answerB"])
+            print("\tAnswer choice 3:", sample["answerC"])
             print("The correct answer is", label)
+            print()
             print("Experiment " + exp_name_1 + "'s choice: " + str(pred_1[i]))
             print("\twith probabilities for each choice:", *prob_1[i])
+            print()
             print("Experiment " + exp_name_2 + "'s choice: " + str(pred_2[i]))
             print("\twith probabilities for each choice:", *prob_2[i])
+            print("---------------------------")
+            print()
         else:
             pass
-	    # print("Agreed on", str(i+1)+"-th sample.")
-        print()
+            # print("Agreed on", str(i+1)+"-th sample.")
     print("=======================")
     count_1_correct = n_samples - count_1_wrong
     count_2_correct = n_samples - count_2_wrong
