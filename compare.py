9cc46f9ffb7db0fb6331cd6f6b7b60554049aff4
Nuan Wen <nuankw@yahoo.com>
2020-02-17 01:20:56 -0800

    strictly better mode

--- compare.py
+++ compare.py
@@ -8,7 +8,8 @@ import argparse
 import jsonlines
 def compare(label_path, jsonl_path,
             exp_name_1, pred_1_path, prob_1_path,
-            exp_name_2, pred_2_path, prob_2_path):
+            exp_name_2, pred_2_path, prob_2_path,
+            exp_1_strictly_better):
     print(jsonl_path)
     labels = [int(i) for i in open(label_path).readlines()]
     qa_reader = jsonlines.open(jsonl_path)
@@ -29,24 +30,23 @@ def compare(label_path, jsonl_path,
         if pred_2[i] != label:
             count_2_wrong += 1
         if pred_1[i] != pred_2[i]:
-            print()
-            print("---------------------------")
-            print("Disagreement on", str(i+1)+"-th sample:")
-            for key in sample:
-                if sample[key] != "":
-                    print("{}: {}".format(key, sample[key]))
-            print("The correct answer is", label)
-            print()
-            print("Experiment " + exp_name_1 + "'s choice: " + str(pred_1[i]))
-            print("\twith probabilities for each choice:", *prob_1[i])
-            print()
-            print("Experiment " + exp_name_2 + "'s choice: " + str(pred_2[i]))
-            print("\twith probabilities for each choice:", *prob_2[i])
-            print("---------------------------")
-            print()
-        else:
-            pass
-            # print("Agreed on", str(i+1)+"-th sample.")
+            if not exp_1_strictly_better or pred_1[i] == label:
+                print()
+                print("---------------------------")
+                print("Disagreement on", str(i+1)+"-th sample:")
+                for key in sample:
+                    if sample[key] != "":
+                        print("{}: {}".format(key, sample[key]))
+                print("The correct answer is", label)
+                print()
+                print("Experiment " + exp_name_1 + "'s choice: " + str(pred_1[i]))
+                print("\twith probabilities for each choice:", *prob_1[i])
+                print()
+                print("Experiment " + exp_name_2 + "'s choice: " + str(pred_2[i]))
+                print("\twith probabilities for each choice:", *prob_2[i])
+                print("---------------------------")
+                print()
+
     print("=======================")
     count_1_correct = n_samples - count_1_wrong
     count_2_correct = n_samples - count_2_wrong
@@ -63,11 +63,18 @@ def main():
     parser.add_argument("--prob_1_path", required=True)
     parser.add_argument("--prob_2_path", required=True)
     parser.add_argument("--jsonl_path", required=True)
+    parser.add_argument("--exp_1_strictly_better", action="store_true")
     args = parser.parse_args()
-    compare(args.label_path, args.jsonl_path,
-            args.exp_name_1, args.pred_1_path, args.prob_1_path,
-            args.exp_name_2, args.pred_2_path, args.prob_2_path)
-
+    if args.exp_1_strictly_better:
+        compare(args.label_path, args.jsonl_path,
+                args.exp_name_1, args.pred_1_path, args.prob_1_path,
+                args.exp_name_2, args.pred_2_path, args.prob_2_path,
+                True)
+    else:
+        compare(args.label_path, args.jsonl_path,
+                args.exp_name_1, args.pred_1_path, args.prob_1_path,
+                args.exp_name_2, args.pred_2_path, args.prob_2_path,
+                False)
 
 if __name__ == "__main__":
     main()
